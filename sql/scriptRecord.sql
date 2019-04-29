677c2162904649dc35e034bbc285f3289593ab64
Firman Sandy P <firmansandyp@gmail.com>
2019-04-29 13:20:06 +0700

    Menambahkan script sql (tabel, record, dan stored procedure)

---
+++ sql/scriptRecord.sql
@@ -0,0 +1,58 @@
+--Nama basis data: perpustakaantbd
+
+--Buku
+insert into buku(judulbuku)
+values ('Algiritma dan Struktur Data');
+insert into buku(judulbuku)
+values ('Introduction to Algorithm');
+insert into buku(judulbuku)
+values ('Programming for Dummies');
+insert into buku(judulbuku)
+values ('Manage the Economy');
+insert into buku(judulbuku)
+values ('Network Security');
+
+--Pengarang
+insert into pengarang(namapengarang)
+values ('Alvinus Sutendy');
+insert into pengarang(namapengarang)
+values ('Petrus Gumal');
+insert into pengarang(namapengarang)
+values ('Johanes Irwan');
+insert into pengarang(namapengarang)
+values ('Firman Sandy Prayitno');
+insert into pengarang(namapengarang)
+values ('Steven Rogerson');
+
+--Kata
+insert into kata(namakata,idf)
+values ('Algoritma',0.43);
+insert into kata(namakata,idf)
+values ('dan',0.06);
+insert into kata(namakata,idf)
+values ('Struktur',3.34);
+insert into kata(namakata,idf)
+values ('Data',1.45);
+insert into kata(namakata,idf)
+values ('Network',1.4);
+
+--Tag
+insert into tag(namatag)
+values 
+
+--BukuPengarang
+
+--BukuTag
+
+--BukuKata
+
+--Eksemplar
+
+
+--Anggota
+
+--Denda
+
+--Pemesanan
+
+--Peminjaman
